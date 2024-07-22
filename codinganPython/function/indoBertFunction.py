46db43a1104baac1dc38db10449000331938ee4a
Muhammad Aulia <adheaulia9578@gmail.com>
2024-07-22 21:45:42 +0700

    delete library transformers

--- codinganPython/function/indoBertFunction.py
+++ codinganPython/function/indoBertFunction.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import tensorflow as tf
-from transformers import BertTokenizer, TFBertForSequenceClassification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from imblearn.over_sampling import RandomOverSampler
